8a261034bef4cad59372bd9b155d3677cbbbcdc7
brooze <56531358+purusoth-lw@users.noreply.github.com>
2021-05-05 12:49:43 +0530

    Add files via upload

---
+++ 2.Basic Program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //std::cout<<"Livewire"<<std::endl;//<< --- stream insertion operator
+    cout<<"Livewire"<<endl;
+        /*
+    endl----end line
+    */
+    return 0;
+}
